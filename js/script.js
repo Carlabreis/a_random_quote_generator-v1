10c339640f7351d4cb5bcd733e93077733c33d34
Carla Borges Reis <carlareis.dev.com>
2023-09-09 17:35:16 -0700

    updates

--- js/script.js
+++ js/script.js
@@ -7,27 +7,84 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
 
 
+// array of objects to store quotes data
+const quotes = [
+  {
+    quote: "And now here is my secret, a very simple secret: It is only with the heart that one can see rightly; what is essential is invisible to the eye.",
+    source: "Antoine de Saint-Exupéry",
+    citation: "The Little Prince",
+    year: "1943"
+  },
+  {
+    quote: "But the conceited man did not hear him. Conceited people never hear anything but praise.",
+    source: "Antoine de Saint-Exupéry",
+    citation: "The Little Prince",
+    year: "1943"
+  },
+  {
+    quote: "Well, I must endure the presence of a few caterpillars if I wish to become acquainted with the butterflies.",
+    source: "Antoine de Saint-Exupéry",
+    citation: "The Little Prince",
+    year: "1943"
+  },
+  {
+    quote: `Where are the people?” resumed the little prince at last. “It’s a little lonely in the desert…” “It is lonely when you're among people, too,” said the snake.`,
+    source: "Antoine de Saint-Exupéry",
+    citation: "The Little Prince",
+    year: "1943"
+  },
+  {
+    quote: "He was only a fox like a hundred thousand other foxes. But I have made him my friend, and now he is unique in all the world.",
+    source: "Antoine de Saint-Exupéry",
+    citation: "The Little Prince",
+    year: "1943"
+  }
+];
 
-/***
- * `getRandomQuote` function
-***/
+
+// returns a random quote object from array of objects
+
+function getRandomQuote(quotes) {
+  const randomNumber = Math.floor(Math.random() * quotes.length);
+  return quotes[randomNumber];
+}
 
 
+//function that gets called when click button
 
-/***
- * `printQuote` function
-***/
+function printQuote() {
+  let randomQuote = getRandomQuote(quotes);
+  const htmlString = `
+    <p class="quote"> ${randomQuote.quote} </p>
+    <p class="source"> ${randomQuote.source}`
+      if (randomQuote.citation) {
+        `<span class="citation">${randomQuote.citation}</span>`
+        }
+      if (randomQuote.year) {
+        `<span class="year">${randomQuote.year}</span>`
+        }
+    `</p>`
+  ;
 
+  document.getElementById('quote-box').innerHTML = htmlString; 
 
+  
+}
 
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+// change background color
+// code inspired by https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
+function changeBkgd() {
+  const a = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const c = Math.floor(Math.random() * 256);
+  
+  document.body.style.background = `rgb(${a}, ${b}, ${c})`;
+}
 
+// event listener to change background collor on button click
+document.getElementById('load-quote').addEventListener("click", changeBkgd, false)
+
+// event listener to print quote on button click
 document.getElementById('load-quote').addEventListener("click", printQuote, false);

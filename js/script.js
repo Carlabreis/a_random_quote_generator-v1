f471e064d31b9ca078a73ce8985b2eaf71c3d206
Carla Borges Reis <carlareis.dev.com>
2023-09-09 18:37:32 -0700

    added extra features for exceed expectations grade

--- js/script.js
+++ js/script.js
@@ -14,14 +14,11 @@ const quotes = [
   {
     quote: "And now here is my secret, a very simple secret: It is only with the heart that one can see rightly; what is essential is invisible to the eye.",
     source: "Antoine de Saint-Exupéry",
-    citation: "The Little Prince",
-    year: "1943"
+    citation: "The Little Prince"
   },
   {
     quote: "But the conceited man did not hear him. Conceited people never hear anything but praise.",
-    source: "Antoine de Saint-Exupéry",
-    citation: "The Little Prince",
-    year: "1943"
+    source: "Antoine de Saint-Exupéry"
   },
   {
     quote: "Well, I must endure the presence of a few caterpillars if I wish to become acquainted with the butterflies.",
@@ -32,14 +29,14 @@ const quotes = [
   {
     quote: `Where are the people?” resumed the little prince at last. “It’s a little lonely in the desert…” “It is lonely when you're among people, too,” said the snake.`,
     source: "Antoine de Saint-Exupéry",
-    citation: "The Little Prince",
     year: "1943"
   },
   {
     quote: "He was only a fox like a hundred thousand other foxes. But I have made him my friend, and now he is unique in all the world.",
     source: "Antoine de Saint-Exupéry",
     citation: "The Little Prince",
-    year: "1943"
+    year: "1943",
+    tags: ["friendship", "love"]
   }
 ];
 
@@ -56,25 +53,28 @@ function getRandomQuote(quotes) {
 
 function printQuote() {
   let randomQuote = getRandomQuote(quotes);
-  const htmlString = `
+  let htmlString = `
     <p class="quote"> ${randomQuote.quote} </p>
     <p class="source"> ${randomQuote.source}`
       if (randomQuote.citation) {
-        `<span class="citation">${randomQuote.citation}</span>`
+        htmlString += `<span class="citation">${randomQuote.citation}</span>`
         }
       if (randomQuote.year) {
-        `<span class="year">${randomQuote.year}</span>`
+        htmlString += `<span class="year">${randomQuote.year}</span>`
         }
+      if (randomQuote.tags) {
+        htmlString += `<span class="year">${randomQuote.tags.join(', ')}</span>`
+      }
     `</p>`
   ;
 
-  document.getElementById('quote-box').innerHTML = htmlString; 
-
-  
+  document.getElementById('quote-box').innerHTML = htmlString;
 }
 
+
 // change background color
-// code inspired by https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
+// function inspired by https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
+
 function changeBkgd() {
   const a = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
@@ -84,7 +84,10 @@ function changeBkgd() {
 }
 
 // event listener to change background collor on button click
-document.getElementById('load-quote').addEventListener("click", changeBkgd, false)
+document.getElementById('load-quote').addEventListener("click", changeBkgd, false);
+
+//auto-refresh quotes every 20 seconds
+setInterval(() => {printQuote(), changeBkgd()}, 20000);
 
 // event listener to print quote on button click
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
